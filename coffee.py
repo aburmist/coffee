a7ab98521c36289f5075fbbfe945316eb899361f
aburmist <aburmist@gmail.com>
2024-06-19 19:29:28 -0400

    Split the form

--- coffee.py
+++ coffee.py
@@ -65,28 +65,34 @@ def calc_brew_ratio(brew_method, coffee_weight):
 def main():
     st.title('Coffee Taster ☕')
 
-    with st.form(key='my_form'):
-        st.subheader('**Record your coffee**')
-        # Input widgets
-        col1, col2, col3, col4 = st.columns(4)
+    st.subheader('**Record your coffee**')
+
+    with st.form(key='input_form'):
+        col1, col2 = st.columns(2)
         with col1:
-            coffee_grind = st.number_input('Grind size', max_value=40, min_value=0, step=1)
-            brew_method = st.selectbox('Brew method', ['Espresso', 'Aeropress', 'Pour Over', 'Clever', 'French Press', 'Moka', 'Drip'])  
+            brew_method = st.selectbox('Brew method', ['Espresso', 'Aeropress', 'Pour Over', 'Clever', 'French Press', 'Moka', 'Drip'])
+            water_temperature = st.selectbox('Temperature', ['175 Green', '185 White', '190 Oolong', '200 FrenchPress', 'Boil'])
         with col2:
             coffee_weight = st.number_input('Coffee weight (g)', step=0.1)
-            water_weight = st.number_input('Water weight (g)', step=10)
-        with col3:
-            water_temperature = st.selectbox('Temperature', ['175 Green', '185 White', '190 Oolong', '200 FrenchPress', 'Boil'])
-            rating = st.selectbox('Rating', ['⭐️', '⭐️⭐️', '⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️⭐️'])
-        with col4:
-            brew_time = st.number_input('Brew time (s)', step=1)
-            comment = st.text_input("Comment", help="Coffee brand, tasting notes, etc.", placeholder="Tasting notes...")
-        submitted = st.form_submit_button('Submit')
+            coffee_grind = st.number_input('Grind size', max_value=40, min_value=0, step=1)
+        captured = st.form_submit_button('Submit')
 
+    if captured:
+        ratio, brew_weight = calc_brew_ratio(brew_method, coffee_weight)
+        st.write(f'Ideal Brew Ratio for {brew_method} is {ratio} and should weight {int(brew_weight)} grams.')
+
+    with st.form(key='my_form'):
+        col1, col2 = st.columns(2)
+        with col1:
+            brew_time = st.number_input('Brew time (s)', step=1)
+            water_weight = st.number_input('Water weight (g)', step=10)
+        with col2:
+            rating = st.selectbox('Rating', ['⭐️', '⭐️⭐️', '⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️', '⭐️⭐️⭐️⭐️⭐️'])
+            comment = st.text_input("Comment", help="Coffee brand, tasting notes, etc.", placeholder="Tasting notes...")
+        submitted = st.form_submit_button('Review')
+   
     if submitted:
         submit(coffee_grind, brew_method,coffee_weight, water_weight, water_temperature, brew_time, rating, comment)
-        ratio, brew_weight = calc_brew_ratio(brew_method, coffee_weight)
-        st.write(f'Ideal Brew Ratio for {brew_method} is {ratio} and should weight {int(brew_weight)} ml')
         st.markdown(f'''
             ☕ You have tasted a coffee with the following characteristics:
             - Weight (g): `{coffee_weight}`
